8a1975aa2a61ac13c9b389bb63de79e92e36205d
Анатолий Селявко <a.selyavko@ecmgroup.pro>
2023-04-30 15:23:10 +0700

    Добавлена реализация интерфейса IAuthDal

--- JobSite/Dal/Auth/AuthDAL.cs
+++ JobSite/Dal/Auth/AuthDAL.cs
@@ -1,6 +1,45 @@
-﻿namespace JobSite.DAL.Auth
+﻿using System;
+using System.Data;
+using Dapper;
+using System.Data.SqlClient;
+using JobSite.DAL.Models;
+
+namespace JobSite.DAL.Auth
 {
-    public class AuthDAL
+    public class AuthDAL : IAuthDAL
     {
+        public async Task<UserModel> GetUser(int id)
+        {
+            using (IDbConnection connection = new SqlConnection(DbHalper.ConnectionString))
+            {
+                connection.Open();
+
+                return await connection.QueryFirstOrDefaultAsync<UserModel>(@"SELECT UserId, Email, Password, Salt, Status
+                                                                            FROM AppUser 
+                                                                            WHERE UserId = @id", new { id = id }) ?? new UserModel();
+            }
+        }
+
+        public async Task<UserModel> GetUser(string email)
+        {
+            using (IDbConnection connection = new SqlConnection(DbHalper.ConnectionString))
+            {
+                connection.Open();
+                return await connection.QueryFirstOrDefaultAsync<UserModel>(@"SELECT UserId, Email, Password, Salt, Status
+                                                                            FROM AppUser 
+                                                                            WHERE Email = @email", new { email = email }) ?? new UserModel(); ;
+            }
+        }
+
+        public async Task<int> CreateUser(UserModel model)
+        {
+            using (IDbConnection connection = new SqlConnection(DbHalper.ConnectionString))
+            {
+                connection.Open();
+                string query = @"INSERT INTO AppUser (Email, Password, Salt, Status)
+                                VALUES (@Email, @Password, @Salt, @Status)";
+                return await connection.ExecuteAsync(query, model);
+            }
+        }
     }
 }
--- JobSite/Dal/Auth/IAuthDAL.cs
+++ JobSite/Dal/Auth/IAuthDAL.cs
@@ -1,6 +1,12 @@
-﻿namespace JobSite.DAL.Auth
+﻿using System;
+using JobSite.DAL.Models;
+
+namespace JobSite.DAL.Auth
 {
-    public class IAuthDAL
+    public interface IAuthDAL
     {
+        Task<UserModel> GetUser(int id);
+        Task<UserModel> GetUser(string email);
+        Task<int> CreateUser(UserModel model);
     }
 }
